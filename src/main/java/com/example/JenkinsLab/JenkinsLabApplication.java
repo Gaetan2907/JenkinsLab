4546a5672aeffc2cfec38447f2b953e7132e21a7
Gaëtan <gaetan.daubresse@gmail.com>
2024-02-22 15:23:19 +0530

    Add comment to trigger jenkins build

---
+++ src/main/java/com/example/JenkinsLab/JenkinsLabApplication.java
@@ -0,0 +1,14 @@
+package com.example.JenkinsLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// Comment
+public class JenkinsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsLabApplication.class, args);
+	}
+
+}
